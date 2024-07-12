82ddc5cdb7ed4f60498f30c1909cb647c5235528
eduperezn <edu.perezn.p@gmail.com>
2024-07-12 16:45:48 -0600

    Se agregan las clases ConsultaMoneda y Moneda que nos ayudaran a comunicarse con la API y obtner los datos de las monedas

---
+++ src/ConsultaMoneda.java
@@ -0,0 +1,27 @@
+import com.google.gson.Gson;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaMoneda {
+
+    public Moneda consultaMoneda(String ConversionRate){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/171b045082952926dbd2c387/latest/"+ConversionRate);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+        try {
+            HttpResponse<String> response = null;
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Moneda.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException("No encontré esa moneda");
+        }
+    }
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
